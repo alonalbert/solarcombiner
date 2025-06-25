bc1c368f025b0521377fe83b3ced1b8521a7d031
Alon Albert <aalbert@google.com>
2025-06-25 10:29:00 -0700

    Notes

---
+++ get-token.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+email=$1
+password="$2"
+envoy_serial=$3
+login_response=$(curl -L -X POST http://enlighten.enphaseenergy.com/login/login.json? -F "user[email]=$email" -F "user[password]=$password")
+
+#echo $login_response
+
+session_id=$(echo "$login_response" | jq -r ".session_id")
+
+web_token=$(curl -L -X POST http://entrez.enphaseenergy.com/tokens -H "Content-Type: application/json" -d "{\"session_id\": \"$session_id\", \"serial_num\": \"$envoy_serial\", \"username\": \"$email\"}")
+
+echo $web_token
+
