52643bed96f14d6b2190203c5a6fa05af259b1e1
Alon Albert <aalbert@google.com>
2025-06-28 20:56:29 -0700

    Use DrawScope.energyArrow

--- android/src/main/java/com/alonalbert/enphase/monitor/ui/components/EnergyArrow.kt
+++ android/src/main/java/com/alonalbert/enphase/monitor/ui/components/EnergyArrow.kt
@@ -13,6 +13,7 @@ import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import com.alonalbert.enphase.monitor.util.px
 import kotlin.math.atan2
 import kotlin.math.cos
 import kotlin.math.sign
@@ -25,7 +26,7 @@ fun DrawScope.energyArrow(
   end: Offset,
   color: Color,
   radius: Dp,
-  strokeWidth: Float = 1.dp.toPx(),
+  strokeWidth: Float = 1.dp.px,
   arrowLengthDp: Dp = 8.dp,
   arrowAngle: Float = 60f,
 ) {
@@ -35,7 +36,7 @@ fun DrawScope.energyArrow(
 
   val angle = atan2(end.y - corner.y, end.x - corner.x)
 
-  val radiusPx = radius.toPx()
+  val radiusPx = radius.px
   val signX1 = sign(corner.x - start.x)
   val signY1 = sign(corner.y - start.y)
   val signX2 = sign(end.x - corner.x)
@@ -46,7 +47,7 @@ fun DrawScope.energyArrow(
 
   val radians = Math.toRadians(arrowAngle.toDouble() / 2)
   val angle1 = angle - radians
-  val arrowLengthPx = arrowLengthDp.toPx()
+  val arrowLengthPx = arrowLengthDp.px
   val arrow1 = Offset(end.x - arrowLengthPx * cos(angle1).toFloat(), end.y - arrowLengthPx * sin(angle1).toFloat())
   val angle2 = angle + radians
   val arrow2 = Offset(end.x - arrowLengthPx * cos(angle2).toFloat(), end.y - arrowLengthPx * sin(angle2).toFloat())
@@ -81,7 +82,7 @@ fun DrawScope.energyArrow(
   start: Offset,
   end: Offset,
   color: Color,
-  strokeWidth: Float = 1.dp.toPx(),
+  strokeWidth: Float = 1.dp.px,
   arrowLengthDp: Dp = 8.dp,
   arrowAngle: Float = 60f,
 ) {
@@ -89,7 +90,7 @@ fun DrawScope.energyArrow(
 
   val radians = Math.toRadians(arrowAngle.toDouble() / 2)
   val angle1 = angle - radians
-  val arrowLengthPx = arrowLengthDp.toPx()
+  val arrowLengthPx = arrowLengthDp.px
   val arrow1 = Offset(end.x - arrowLengthPx * cos(angle1).toFloat(), end.y - arrowLengthPx * sin(angle1).toFloat())
   val angle2 = angle + radians
   val arrow2 = Offset(end.x - arrowLengthPx * cos(angle2).toFloat(), end.y - arrowLengthPx * sin(angle2).toFloat())
@@ -115,42 +116,42 @@ fun DrawScope.energyArrow(
 fun EnergyArrow1() {
   Canvas(modifier = Modifier.fillMaxSize()) {
     energyArrow(
-      start = Offset(200.dp.toPx(), 20.dp.toPx()),
-      end = Offset(200.dp.toPx(), 380.dp.toPx()),
+      start = Offset(200.dp.px, 20.dp.px),
+      end = Offset(200.dp.px, 380.dp.px),
       color = Color.Black,
     )
 
     energyArrow(
-      start = Offset(20.dp.toPx(), 200.dp.toPx()),
-      end = Offset(380.dp.toPx(), 200.dp.toPx()),
+      start = Offset(20.dp.px, 200.dp.px),
+      end = Offset(380.dp.px, 200.dp.px),
       color = Color.Black,
     )
 
     energyArrow(
-      start = Offset(220.dp.toPx(), 20.dp.toPx()),
-      corner = Offset(220.dp.toPx(), 180.dp.toPx()),
-      end = Offset(380.dp.toPx(), 180.dp.toPx()),
+      start = Offset(220.dp.px, 20.dp.px),
+      corner = Offset(220.dp.px, 180.dp.px),
+      end = Offset(380.dp.px, 180.dp.px),
       color = Color.Black,
       radius = 20.dp,
     )
     energyArrow(
-      start = Offset(180.dp.toPx(), 20.dp.toPx()),
-      corner = Offset(180.dp.toPx(), 180.dp.toPx()),
-      end = Offset(20.dp.toPx(), 180.dp.toPx()),
+      start = Offset(180.dp.px, 20.dp.px),
+      corner = Offset(180.dp.px, 180.dp.px),
+      end = Offset(20.dp.px, 180.dp.px),
       color = Color.Black,
       radius = 20.dp,
     )
     energyArrow(
-      start = Offset(220.dp.toPx(), 380.dp.toPx()),
-      corner = Offset(220.dp.toPx(), 220.dp.toPx()),
-      end = Offset(380.dp.toPx(), 220.dp.toPx()),
+      start = Offset(220.dp.px, 380.dp.px),
+      corner = Offset(220.dp.px, 220.dp.px),
+      end = Offset(380.dp.px, 220.dp.px),
       color = Color.Black,
       radius = 20.dp,
     )
     energyArrow(
-      start = Offset(180.dp.toPx(), 380.dp.toPx()),
-      corner = Offset(180.dp.toPx(), 220.dp.toPx()),
-      end = Offset(20.dp.toPx(), 220.dp.toPx()),
+      start = Offset(180.dp.px, 380.dp.px),
+      corner = Offset(180.dp.px, 220.dp.px),
+      end = Offset(20.dp.px, 220.dp.px),
       color = Color.Black,
       radius = 20.dp,
     )
@@ -162,40 +163,40 @@ fun EnergyArrow1() {
 fun EnergyArrow2() {
   Canvas(modifier = Modifier.fillMaxSize()) {
     energyArrow(
-      start = Offset(200.dp.toPx(), 380.dp.toPx()),
-      end = Offset(200.dp.toPx(), 20.dp.toPx()),
+      start = Offset(200.dp.px, 380.dp.px),
+      end = Offset(200.dp.px, 20.dp.px),
       color = Color.Black,
     )
     energyArrow(
-      start = Offset(380.dp.toPx(), 200.dp.toPx()),
-      end = Offset(20.dp.toPx(), 200.dp.toPx()),
+      start = Offset(380.dp.px, 200.dp.px),
+      end = Offset(20.dp.px, 200.dp.px),
       color = Color.Black,
     )
     energyArrow(
-      start = Offset(380.dp.toPx(), 180.dp.toPx()),
-      corner = Offset(220.dp.toPx(), 180.dp.toPx()),
-      end = Offset(220.dp.toPx(), 20.dp.toPx()),
-      color = Color.Black,
-      radius = 20.dp,
-    )
-    energyArrow(
-      start = Offset(20.dp.toPx(), 180.dp.toPx()),
-      corner = Offset(180.dp.toPx(), 180.dp.toPx()),
-      end = Offset(180.dp.toPx(), 20.dp.toPx()),
+      start = Offset(380.dp.px, 180.dp.px),
+      corner = Offset(220.dp.px, 180.dp.px),
+      end = Offset(220.dp.px, 20.dp.px),
       color = Color.Black,
       radius = 20.dp,
     )
     energyArrow(
-      start = Offset(380.dp.toPx(), 220.dp.toPx()),
-      corner = Offset(220.dp.toPx(), 220.dp.toPx()),
-      end = Offset(220.dp.toPx(), 380.dp.toPx()),
+      start = Offset(20.dp.px, 180.dp.px),
+      corner = Offset(180.dp.px, 180.dp.px),
+      end = Offset(180.dp.px, 20.dp.px),
       color = Color.Black,
       radius = 20.dp,
     )
     energyArrow(
-      start = Offset(20.dp.toPx(), 220.dp.toPx()),
-      corner = Offset(180.dp.toPx(), 220.dp.toPx()),
-      end = Offset(180.dp.toPx(), 380.dp.toPx()),
+      start = Offset(380.dp.px, 220.dp.px),
+      corner = Offset(220.dp.px, 220.dp.px),
+      end = Offset(220.dp.px, 380.dp.px),
+      color = Color.Black,
+      radius = 20.dp,
+    )
+    energyArrow(
+      start = Offset(20.dp.px, 220.dp.px),
+      corner = Offset(180.dp.px, 220.dp.px),
+      end = Offset(180.dp.px, 380.dp.px),
       color = Color.Black,
       radius = 20.dp,
     )
