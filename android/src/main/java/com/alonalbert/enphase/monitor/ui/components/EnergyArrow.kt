eac1cd45fe3e163b17abe3de559dc0839872787b
Alon Albert <aalbert@google.com>
2025-06-30 10:42:28 -0700

    Remove old EnergyArrow

--- android/src/main/java/com/alonalbert/enphase/monitor/ui/components/EnergyArrow.kt
+++ android/src/main/java/com/alonalbert/enphase/monitor/ui/components/EnergyArrow.kt
@@ -1,12 +1,29 @@
 package com.alonalbert.enphase.monitor.ui.components
 
+import androidx.compose.animation.core.Animatable
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.RepeatMode
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableFloatStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.BottomCenter
+import androidx.compose.ui.Alignment.Companion.CenterEnd
+import androidx.compose.ui.Alignment.Companion.CenterStart
+import androidx.compose.ui.Alignment.Companion.TopCenter
+import androidx.compose.ui.BiasAlignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.geometry.Rect
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.PathMeasure
 import androidx.compose.ui.graphics.drawscope.DrawScope
 import androidx.compose.ui.graphics.drawscope.Fill
@@ -15,201 +32,180 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import com.alonalbert.enphase.monitor.util.px
-import kotlin.math.atan2
-import kotlin.math.cos
-import kotlin.math.sign
-import kotlin.math.sin
+import kotlin.math.tan
 
-
-fun DrawScope.energyArrow(
-  start: Offset,
-  corner: Offset,
-  end: Offset,
-  color: Color,
-  radius: Dp,
-  strokeWidth: Float = 1.dp.px,
-  arrowLengthDp: Dp = 8.dp,
+@Composable
+fun EnergyArrow(
+  start: Alignment,
+  end: Alignment,
+  color: Color = Color.Black,
+  pad: Dp = 10.dp,
+  radius: Dp = 20.dp,
+  strokeWidth: Dp = 1.dp,
+  arrowLength: Dp = 8.dp,
   arrowAngle: Float = 60f,
-  pointPosition: Float? = null,
+  modifier: Modifier = Modifier
 ) {
-  assert(
-    (corner.x == start.x && corner.y == end.y) || (corner.x == end.x && corner.y == start.y)
-  ) { "Corner $corner is invalid between points $start - $end" }
+  assert(start.isValid())
+  assert(end.isValid())
+  assert(end != start)
 
-  val angle = atan2(end.y - corner.y, end.x - corner.x)
+  val linePath = Path()
 
-  val radiusPx = radius.px
-  val signX1 = sign(corner.x - start.x)
-  val signY1 = sign(corner.y - start.y)
-  val signX2 = sign(end.x - corner.x)
-  val signY2 = sign(end.y - corner.y)
+  val pathMeasure = remember { PathMeasure() }
+  var pathLength by remember { mutableFloatStateOf(0f) }
+  val animatedPosition = remember { Animatable(0f) }
 
-  val line1End = Offset(corner.x - radiusPx * signX1, corner.y - radiusPx * signY1)
-  val line2Start = Offset(corner.x + radiusPx * signX2, corner.y + radiusPx * signY2)
-
-  val radians = Math.toRadians(arrowAngle.toDouble() / 2)
-  val angle1 = angle - radians
-  val arrowLengthPx = arrowLengthDp.px
-  val arrow1 = Offset(end.x - arrowLengthPx * cos(angle1).toFloat(), end.y - arrowLengthPx * sin(angle1).toFloat())
-  val angle2 = angle + radians
-  val arrow2 = Offset(end.x - arrowLengthPx * cos(angle2).toFloat(), end.y - arrowLengthPx * sin(angle2).toFloat())
-
-  val mx = line1End.x + line2Start.x - corner.x
-  val my = line1End.y + line2Start.y - corner.y
-  val rect = Rect(Offset(mx, my), radiusPx)
-
-  val s = sign((end.x - start.x) * (end.y - start.y))
-  val startAngle = if (start.x == corner.x) (signX2 + 1) * 90f else signY2 * -90f
-  val sweepAngle = s * if (start.x == corner.x) -90f else 90f
-
-  val linePath = buildPath {
-    moveTo(start)
-    lineTo(line1End)
-    arcTo(rect, startAngle, sweepAngle, false)
-    lineTo(line2Start)
-    lineTo((arrow1.x + arrow2.x) / 2, (arrow1.y + arrow2.y) / 2)
-  }
-  drawPath(linePath, color, style = Stroke(strokeWidth))
-
-  val pathMeasure = PathMeasure()
-  if (pointPosition != null) {
+  // Set up PathMeasure and get path length.
+  // This LaunchedEffect will run when `path` changes.
+  LaunchedEffect(linePath) {
     pathMeasure.setPath(linePath, false)
-    val position = pathMeasure.getPosition(pathMeasure.length * pointPosition)
-    drawCircle(color, radius = strokeWidth * 2f, center = position)
-    drawCircle(color.copy(alpha = 0.25f), radius = strokeWidth * 4, center = position)
+    pathLength = pathMeasure.length
   }
-  val arrowPath = buildPath {
-    moveTo(end)
-    lineTo(arrow1)
-    lineTo(arrow2)
-    close()
+
+  LaunchedEffect(pathLength) {
+    if (pathLength > 0f) {
+      animatedPosition.snapTo(0f) // Reset to start
+      animatedPosition.animateTo(
+        targetValue = pathLength,
+        animationSpec = infiniteRepeatable(
+          animation = tween(
+            durationMillis = 5_000,
+            easing = LinearEasing
+          ),
+          repeatMode = RepeatMode.Restart
+        )
+      )
+    }
   }
-  drawPath(arrowPath, color, style = Fill)
+
+  Canvas(modifier = modifier.fillMaxSize()) {
+    val hasCorner = start.vBias() != end.vBias() && start.hBias() != end.hBias()
+    val padPx = if (hasCorner) pad.px else 0f
+    val startOffset = start.toOffset(padPx, end)
+    val endOffset = end.toOffset(padPx, start)
+    val length = arrowLength.px
+    val height =  (tan(Math.toRadians(arrowAngle.toDouble() / 2).toFloat()) * length)
+    val arrowBase = Offset(endOffset.x - length * end.hBias(), endOffset.y - length * end.vBias())
+    val arrow1 = Offset(arrowBase.x + height * end.vBias(), arrowBase.y + height * end.hBias())
+    val arrow2 = Offset(arrowBase.x - height * end.vBias(), arrowBase.y - height * end.hBias())
+
+    with(linePath) {
+      moveTo(startOffset)
+      if (hasCorner) {
+        addArc(start, end, padPx, radius.px)
+      }
+      lineTo(arrowBase)
+    }
+    val stroke = strokeWidth.px
+    drawPath(linePath, color, style = Stroke(stroke))
+
+    pathMeasure.setPath(linePath, false)
+    val position = pathMeasure.getPosition(animatedPosition.value)
+    drawCircle(color, radius = stroke * 2f, center = position)
+    drawCircle(color.copy(alpha = 0.25f), radius = stroke * 4, center = position)
+
+    val arrowPath = buildPath {
+      moveTo(endOffset)
+      lineTo(arrow1)
+      lineTo(arrow2)
+      close()
+    }
+    drawPath(arrowPath, color, style = Fill)
+  }
 }
 
-fun DrawScope.energyArrow(
-  start: Offset,
-  end: Offset,
-  color: Color,
-  strokeWidth: Float = 1.dp.px,
-  arrowLengthDp: Dp = 8.dp,
-  arrowAngle: Float = 60f,
+context(drawScope: DrawScope)
+private fun Path.addArc(
+  start: Alignment,
+  end: Alignment,
+  padPx: Float,
+  radius: Float,
 ) {
-  val angle = atan2(end.y - start.y, end.x - start.x)
-
-  val radians = Math.toRadians(arrowAngle.toDouble() / 2)
-  val angle1 = angle - radians
-  val arrowLengthPx = arrowLengthDp.px
-  val arrow1 = Offset(end.x - arrowLengthPx * cos(angle1).toFloat(), end.y - arrowLengthPx * sin(angle1).toFloat())
-  val angle2 = angle + radians
-  val arrow2 = Offset(end.x - arrowLengthPx * cos(angle2).toFloat(), end.y - arrowLengthPx * sin(angle2).toFloat())
-
-  val linePath = buildPath {
-    moveTo(start)
-    lineTo((arrow1.x + arrow2.x) / 2, (arrow1.y + arrow2.y) / 2)
+  with(drawScope) {
+    val dx = start.hBias() + end.hBias()
+    val dy = start.vBias() + end.vBias()
+    val middle = Offset(center.x + padPx * dx, center.y + padPx * dy)
+    val rectCenter = Offset(middle.x + radius * dx, middle.y + radius * dy)
+    val rect = Rect(rectCenter, radius)
+    arcTo(rect, end.getStartAngle(), (start to end).getSweepAngle(), false)
   }
-  drawPath(linePath, color, style = Stroke(strokeWidth))
+}
 
-  val arrowPath = buildPath {
-    moveTo(end)
-    lineTo(arrow1)
-    lineTo(arrow2)
-    close()
+private fun Alignment.isValid() =
+  this == TopCenter || this == BottomCenter || this == CenterStart || this == CenterEnd
+
+private fun Alignment.hBias() = (this as BiasAlignment).horizontalBias
+private fun Alignment.vBias() = (this as BiasAlignment).verticalBias
+
+context(drawScope: DrawScope)
+private fun Alignment.toOffset(pad: Float, other: Alignment) =
+  with(drawScope) {
+    Offset(
+      center.x + hBias() * center.x + pad * other.hBias(),
+      center.y + vBias() * center.y + pad * other.vBias(),
+    )
   }
-  drawPath(arrowPath, color, style = Fill)
 
+private fun Pair<Alignment, Alignment>.getSweepAngle() =
+  (first.hBias() + first.vBias()) * (second.hBias() - second.vBias()) * 90f
+
+private fun Alignment.getStartAngle() = (hBias() + 1) * (hBias() - vBias()) * 90f
+
+@Preview(widthDp = 400, heightDp = 400)
+@Composable
+fun EnergyArrowPreview1() {
+  EnergyArrow(
+    start = TopCenter,
+    end = BottomCenter,
+  )
+  EnergyArrow(
+    start = CenterStart,
+    end = CenterEnd,
+  )
+  EnergyArrow(
+    start = TopCenter,
+    end = CenterEnd,
+  )
+  EnergyArrow(
+    start = TopCenter,
+    end = CenterStart,
+  )
+  EnergyArrow(
+    start = BottomCenter,
+    end = CenterEnd,
+  )
+  EnergyArrow(
+    start = BottomCenter,
+    end = CenterStart,
+  )
 }
 
 @Preview(widthDp = 400, heightDp = 400)
 @Composable
-fun EnergyArrow1() {
-  Canvas(modifier = Modifier.fillMaxSize()) {
-    energyArrow(
-      start = Offset(200.dp.px, 20.dp.px),
-      end = Offset(200.dp.px, 380.dp.px),
-      color = Color.Black,
-    )
-
-    energyArrow(
-      start = Offset(20.dp.px, 200.dp.px),
-      end = Offset(380.dp.px, 200.dp.px),
-      color = Color.Black,
-    )
-
-    energyArrow(
-      start = Offset(220.dp.px, 20.dp.px),
-      corner = Offset(220.dp.px, 180.dp.px),
-      end = Offset(380.dp.px, 180.dp.px),
-      color = Color.Black,
-      radius = 20.dp,
-      pointPosition = .75f,
-    )
-    energyArrow(
-      start = Offset(180.dp.px, 20.dp.px),
-      corner = Offset(180.dp.px, 180.dp.px),
-      end = Offset(20.dp.px, 180.dp.px),
-      color = Color.Black,
-      radius = 20.dp,
-    )
-    energyArrow(
-      start = Offset(220.dp.px, 380.dp.px),
-      corner = Offset(220.dp.px, 220.dp.px),
-      end = Offset(380.dp.px, 220.dp.px),
-      color = Color.Black,
-      radius = 20.dp,
-    )
-    energyArrow(
-      start = Offset(180.dp.px, 380.dp.px),
-      corner = Offset(180.dp.px, 220.dp.px),
-      end = Offset(20.dp.px, 220.dp.px),
-      color = Color.Black,
-      radius = 20.dp,
-    )
-  }
+fun EnergyArrowPreview2() {
+  EnergyArrow(
+    start = BottomCenter,
+    end = TopCenter,
+  )
+  EnergyArrow(
+    start = CenterEnd,
+    end = CenterStart,
+  )
+  EnergyArrow(
+    start = CenterEnd,
+    end = TopCenter,
+  )
+  EnergyArrow(
+    start = CenterStart,
+    end = TopCenter,
+  )
+  EnergyArrow(
+    start = CenterEnd,
+    end = BottomCenter,
+  )
+  EnergyArrow(
+    start = CenterStart,
+    end = BottomCenter,
+  )
 }
-
-@Preview(widthDp = 400, heightDp = 400)
-@Composable
-fun EnergyArrow2() {
-  Canvas(modifier = Modifier.fillMaxSize()) {
-    energyArrow(
-      start = Offset(200.dp.px, 380.dp.px),
-      end = Offset(200.dp.px, 20.dp.px),
-      color = Color.Black,
-    )
-    energyArrow(
-      start = Offset(380.dp.px, 200.dp.px),
-      end = Offset(20.dp.px, 200.dp.px),
-      color = Color.Black,
-    )
-    energyArrow(
-      start = Offset(380.dp.px, 180.dp.px),
-      corner = Offset(220.dp.px, 180.dp.px),
-      end = Offset(220.dp.px, 20.dp.px),
-      color = Color.Black,
-      radius = 20.dp,
-    )
-    energyArrow(
-      start = Offset(20.dp.px, 180.dp.px),
-      corner = Offset(180.dp.px, 180.dp.px),
-      end = Offset(180.dp.px, 20.dp.px),
-      color = Color.Black,
-      radius = 20.dp,
-    )
-    energyArrow(
-      start = Offset(380.dp.px, 220.dp.px),
-      corner = Offset(220.dp.px, 220.dp.px),
-      end = Offset(220.dp.px, 380.dp.px),
-      color = Color.Black,
-      radius = 20.dp,
-    )
-    energyArrow(
-      start = Offset(20.dp.px, 220.dp.px),
-      corner = Offset(180.dp.px, 220.dp.px),
-      end = Offset(180.dp.px, 380.dp.px),
-      color = Color.Black,
-      radius = 20.dp,
-    )
-  }
-}
-
