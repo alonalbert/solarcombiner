48b7abd534cc16d02fcbbf2270ec5cd185c48316
Alon Albert <aalbert@google.com>
2025-07-09 13:45:28 -0700

    Cancel job on setDay

--- android/src/main/java/com/alonalbert/enphase/monitor/ui/energy/EnergyViewModel.kt
+++ android/src/main/java/com/alonalbert/enphase/monitor/ui/energy/EnergyViewModel.kt
@@ -9,6 +9,7 @@ import com.alonalbert.solar.combiner.enphase.Enphase.CacheMode.NO_CACHE
 import com.alonalbert.solar.combiner.enphase.model.DailyEnergy
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.Deferred
+import kotlinx.coroutines.Job
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
@@ -19,15 +20,18 @@ import javax.inject.Inject
 class EnergyViewModel @Inject constructor(
   private val enphaseAsync: Deferred<Enphase>,
 ) : ViewModel() {
-  private suspend fun enphase() = enphaseAsync.await()
+  private var job: Job? = null
 
   private val dailyEnergyFlow: MutableStateFlow<DailyEnergy?> = MutableStateFlow(null)
+
   val dailyEnergyState: StateFlow<DailyEnergy?> = dailyEnergyFlow.stateIn(viewModelScope, null)
 
+  private suspend fun enphase() = enphaseAsync.await()
+
   fun setDay(day: LocalDate) {
-    viewModelScope.launch {
+    job?.cancel()
+    job = viewModelScope.launch {
       dailyEnergyFlow.value = enphase().getDailyEnergy(day, CACHE_ONLY)
-      dailyEnergyFlow.value = enphase().getDailyEnergy(day, NO_CACHE)
-    }
+      dailyEnergyFlow.value = enphase().getDailyEnergy(day, NO_CACHE)    }
   }
 }
