02b635d24babee1a05458ec5d2ec996064818031
Alon Albert <aalbert@google.com>
2025-07-23 16:26:47 -0700

    reserve log file

--- android/src/main/java/com/alonalbert/enphase/monitor/ui/reserve/ReserveManagerWorker.kt
+++ android/src/main/java/com/alonalbert/enphase/monitor/ui/reserve/ReserveManagerWorker.kt
@@ -13,7 +13,14 @@ import dagger.assisted.AssistedInject
 import kotlinx.coroutines.Deferred
 import timber.log.Timber
 import java.io.IOException
+import java.io.PrintWriter
+import java.nio.file.StandardOpenOption.APPEND
+import java.nio.file.StandardOpenOption.CREATE
+import java.time.LocalDateTime
 import java.time.LocalTime
+import java.time.format.DateTimeFormatter.ISO_LOCAL_DATE_TIME
+import kotlin.io.path.writer
+import kotlin.text.Charsets.UTF_8
 
 @HiltWorker
 class ReserveManagerWorker @AssistedInject constructor(
@@ -25,20 +32,24 @@ class ReserveManagerWorker @AssistedInject constructor(
 
   override suspend fun doWork(): Result {
     Timber.i("ReserveManagerWorker: doWork()")
+    logWork("ReserveManagerWorker: doWork()")
     return try {
       if (!NetworkChecker.checkNetwork(applicationContext)) {
         Timber.w("Network connected but not validated. Might be an issue in Doze. Retrying.")
+        logWork("Network connected but not validated. Might be an issue in Doze. Retrying.")
         return Result.retry()
       }
 
       val config = db.reserveConfigDao().getReserveConfig()
       if (config == null) {
         Timber.w("Reserve configuration not found")
+        logWork("Reserve configuration not found")
         return Result.failure()
       }
       val settings = db.settingsDao().getSettings()
       if (settings == null) {
         Timber.w("Settings not found")
+        logWork("Settings not found")
         return Result.failure()
       }
 
@@ -53,13 +64,24 @@ class ReserveManagerWorker @AssistedInject constructor(
       enphase.ensureLogin(settings.email, settings.password)
       val result = enphase.setBatteryReserve(settings.mainSiteId, reserve)
       Timber.d("Setting reserve to $reserve: $result")
+      logWork("Setting reserve to $reserve: $result")
       Result.success()
     } catch (e: IOException) {
       Timber.e(e, "Failed to set reserve")
+      logWork("Failed to set reserve", e)
       Result.retry()
     } catch (e: Exception) {
       Timber.e(e, "Failed to set reserve")
+      logWork("Failed to set reserve", e)
       Result.failure()
+    }
+  }
+
+  private fun logWork(message: String, e: Exception? = null) {
+    val logFile = applicationContext.cacheDir.toPath().resolve("reserve.log")
+    logFile.writer(UTF_8, APPEND, CREATE).use {
+      it.write("${LocalDateTime.now().format(ISO_LOCAL_DATE_TIME)}: $message\n")
+      e?.printStackTrace(PrintWriter(it))
     }
   }
 
