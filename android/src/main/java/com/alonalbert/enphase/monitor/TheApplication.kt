7c951bdd31e2e4291ab9a2c06119a8d0b5051bed
Alon Albert <aalbert@google.com>
2025-08-11 12:34:23 -0700

    Remove Work Deps

--- android/src/main/java/com/alonalbert/enphase/monitor/TheApplication.kt
+++ android/src/main/java/com/alonalbert/enphase/monitor/TheApplication.kt
@@ -1,18 +1,13 @@
 package com.alonalbert.enphase.monitor
 
 import android.app.Application
-import androidx.hilt.work.HiltWorkerFactory
 import com.alonalbert.enphase.monitor.services.AlarmReceiver
 import dagger.hilt.android.HiltAndroidApp
 import timber.log.Timber
-import javax.inject.Inject
 
 
 @HiltAndroidApp
 class TheApplication : Application() {
-  @Inject
-  lateinit var workerFactory: HiltWorkerFactory
-
   override fun onCreate() {
     super.onCreate()
 
