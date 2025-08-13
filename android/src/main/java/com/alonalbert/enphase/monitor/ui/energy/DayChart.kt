daabb22016124417cc9c06d8a735c07598f71adc
Alon Albert <aalbert@google.com>
2025-08-13 08:54:39 -0700

    Move Marker Formatter into DayChart

--- android/src/main/java/com/alonalbert/enphase/monitor/ui/energy/DayChart.kt
+++ android/src/main/java/com/alonalbert/enphase/monitor/ui/energy/DayChart.kt
@@ -1,5 +1,10 @@
 package com.alonalbert.enphase.monitor.ui.energy
 
+import android.content.Context
+import android.text.SpannableStringBuilder
+import android.text.Spanned.SPAN_EXCLUSIVE_EXCLUSIVE
+import android.text.style.ForegroundColorSpan
+import android.text.style.TabStopSpan
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.height
@@ -14,10 +19,15 @@ import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.core.text.buildSpannedString
 import com.alonalbert.enphase.monitor.R
+import com.alonalbert.enphase.monitor.enphase.util.kw
+import com.alonalbert.enphase.monitor.enphase.util.zerofy
 import com.alonalbert.enphase.monitor.repository.DayData
 import com.alonalbert.enphase.monitor.ui.energy.ProductionSplit.EXPORT
 import com.alonalbert.enphase.monitor.ui.energy.ProductionSplit.MAIN
+import com.alonalbert.enphase.monitor.ui.theme.colorOf
+import com.alonalbert.enphase.monitor.ui.theme.toInt
 import com.alonalbert.enphase.monitor.util.seriesOrEmpty
 import com.patrykandpatrick.vico.compose.cartesian.CartesianChartHost
 import com.patrykandpatrick.vico.compose.cartesian.axis.rememberAxisLabelComponent
@@ -31,6 +41,7 @@ import com.patrykandpatrick.vico.compose.cartesian.rememberCartesianChart
 import com.patrykandpatrick.vico.compose.cartesian.rememberVicoZoomState
 import com.patrykandpatrick.vico.compose.common.component.rememberLineComponent
 import com.patrykandpatrick.vico.compose.common.fill
+import com.patrykandpatrick.vico.core.cartesian.CartesianDrawingContext
 import com.patrykandpatrick.vico.core.cartesian.axis.HorizontalAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis
 import com.patrykandpatrick.vico.core.cartesian.axis.VerticalAxis.HorizontalLabelPosition.Inside
@@ -40,6 +51,9 @@ import com.patrykandpatrick.vico.core.cartesian.data.lineSeries
 import com.patrykandpatrick.vico.core.cartesian.layer.ColumnCartesianLayer
 import com.patrykandpatrick.vico.core.cartesian.layer.LineCartesianLayer
 import com.patrykandpatrick.vico.core.cartesian.layer.LineCartesianLayer.LineStroke
+import com.patrykandpatrick.vico.core.cartesian.marker.CartesianMarker
+import com.patrykandpatrick.vico.core.cartesian.marker.ColumnCartesianLayerMarkerTarget
+import com.patrykandpatrick.vico.core.cartesian.marker.DefaultCartesianMarker.ValueFormatter
 import kotlinx.coroutines.runBlocking
 
 private val EMPTY = List(96) { 0.0 }
@@ -116,7 +130,7 @@ private fun DayChart(
               )
             },
           ),
-        marker = rememberMarker(DailyEnergyValueFormatter(LocalContext.current), lineCount = 4),
+        marker = rememberMarker(DayMarkerValueFormatter(LocalContext.current), lineCount = 4),
         layerPadding = { cartesianLayerPadding(scalableStart = 0.dp, scalableEnd = 0.dp) },
       ),
     modelProducer = modelProducer,
@@ -149,6 +163,49 @@ private suspend fun CartesianChartModelProducer.runTransaction(
       }
       series(values.map { it * 4 })
     }
+  }
+}
+
+class DayMarkerValueFormatter(
+  private val androidContext: Context,
+) : ValueFormatter {
+  override fun format(
+    context: CartesianDrawingContext,
+    targets: List<CartesianMarker.Target>
+  ): CharSequence {
+    with(androidContext) {
+      val solarColor = colorOf(R.color.solar)
+      val gridColor = colorOf(R.color.grid)
+      val batteryColor = colorOf(R.color.battery)
+      val consumptionColor = colorOf(R.color.consumption)
+      return buildSpannedString {
+        targets.filterIsInstance<ColumnCartesianLayerMarkerTarget>().forEach { target ->
+          val columns = target.columns
+          val produced = columns[0].entry.y
+          val consumed = -columns[1].entry.y
+          val grid = columns[2].entry.y
+          val battery = columns[3].entry.y
+          appendEnergyColumn("Produced", produced, solarColor)
+          when (grid >= 0) {
+            true -> appendEnergyColumn("Imported", grid, gridColor)
+            false -> appendEnergyColumn("Exported", -grid, gridColor)
+          }
+          when (battery >= 0) {
+            true -> appendEnergyColumn("Discharged", battery, batteryColor)
+            false -> appendEnergyColumn("Charged", -battery, batteryColor)
+          }
+          appendEnergyColumn("Consumed", consumed, consumptionColor)
+
+          setSpan(TabStopSpan.Standard(100), 0, length, SPAN_EXCLUSIVE_EXCLUSIVE)
+        }
+      }
+    }
+  }
+}
+
+private fun SpannableStringBuilder.appendEnergyColumn(name: String, value: Double, color: Color) {
+  if (value.zerofy() != 0.0) {
+    append("$name:\t${value.kw}\n", ForegroundColorSpan(color.toInt()), SPAN_EXCLUSIVE_EXCLUSIVE)
   }
 }
 
--- android/src/main/java/com/alonalbert/enphase/monitor/ui/energy/MonthChart.kt
+++ android/src/main/java/com/alonalbert/enphase/monitor/ui/energy/MonthChart.kt
@@ -9,7 +9,6 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -105,7 +104,7 @@ private fun MonthChart(
               )
             },
           ),
-        marker = rememberMarker(DailyEnergyValueFormatter(LocalContext.current), lineCount = 4),
+//        marker = rememberMarker(DailyEnergyValueFormatter(LocalContext.current), lineCount = 4),
         layerPadding = { cartesianLayerPadding(scalableStart = 0.dp, scalableEnd = 0.dp) },
       ),
     modelProducer = modelProducer,
