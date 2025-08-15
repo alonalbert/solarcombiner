65619e16ddef13c817fdc5ff999ae873e37fbc04
Alon Albert <aalbert@google.com>
2025-08-15 09:07:44 -0700

    axis formatter tweak

--- android/src/main/java/com/alonalbert/enphase/monitor/ui/energy/Axis.kt
+++ android/src/main/java/com/alonalbert/enphase/monitor/ui/energy/Axis.kt
@@ -3,7 +3,7 @@ package com.alonalbert.enphase.monitor.ui.energy
 import com.patrykandpatrick.vico.core.cartesian.data.CartesianValueFormatter
 import java.text.DecimalFormat
 
-val DecimalValueFormatter = CartesianValueFormatter.decimal(DecimalFormat("#"))
+val DecimalValueFormatter = CartesianValueFormatter.decimal(DecimalFormat("#.#"))
 
 val TimeOfDayAxisValueFormatter = CartesianValueFormatter { context, x, _ ->
   when (val h = x.toInt() / 4) {
