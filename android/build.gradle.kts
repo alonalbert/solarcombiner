d56ded0efe080986c8836a270dbc3cfed445b9c0
Alon Albert <aalbert@google.com>
2025-12-21 07:48:32 +0200

    Packaging excludes

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -52,6 +52,9 @@ android {
     resources {
       excludes += "/META-INF/NOTICE.md"
       excludes += "/META-INF/{AL2.0,LGPL2.1}"
+      excludes += "/META-INF/INDEX.LIST"
+      excludes += "/META-INF/DEPENDENCIES"
+      excludes += "/META-INF/io.netty.versions.properties"
     }
   }
 }
