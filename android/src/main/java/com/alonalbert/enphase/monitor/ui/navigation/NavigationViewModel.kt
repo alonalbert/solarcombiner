56223f00f5e40e2e7bd61ec73021f1745bc9ca36
Alon Albert <aalbert@google.com>
2026-01-16 10:25:52 -0800

    Try to fix crash on launch

--- android/src/main/java/com/alonalbert/enphase/monitor/ui/navigation/NavigationViewModel.kt
+++ android/src/main/java/com/alonalbert/enphase/monitor/ui/navigation/NavigationViewModel.kt
@@ -14,33 +14,22 @@ import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.distinctUntilChanged
 import kotlinx.coroutines.flow.map
 import kotlinx.coroutines.launch
-import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class NavigationViewModel
 @Inject constructor(
-  private val db: AppDatabase,
+  db: AppDatabase,
   private val repository: Repository,
 ) : ViewModel() {
 
   val loginState: StateFlow<LoginState> = db.loginInfoDao().flow().distinctUntilChanged().map {
-    when (it?.isValid()) {
-      true -> updateLoginInfo()
+    when {
+      it == null -> Loading
+      it.isValid() -> repository.updateEnphaseConfig(it).let { LoggedIn }
       else -> LoggedOut
     }
   }.stateIn(viewModelScope, Loading)
-
-  suspend fun updateLoginInfo(): LoggedIn {
-    val loginInfo = db.loginInfoDao().get() ?: return LoggedIn
-    try {
-      repository.updateEnphaseConfig(loginInfo)
-      Timber.i("Updated Enphase config")
-    } catch (e: Exception) {
-      Timber.w(e, "Failed to update Enphase config")
-    }
-    return LoggedIn
-  }
 
   fun updateReserveConfig(reserveConfig: ReserveConfig) {
     viewModelScope.launch {
